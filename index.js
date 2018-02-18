f3b6b375095afdcdf0e6aaf2f0710976684cf01e
Peter Ayres <peter.ayresmusic@gmail.com>
2018-02-18 17:04:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function sayHiToGrandma(string) {
      return "I can't hear you!"}
   else if (string.toUpperCase() === string) { 
      return "YES INDEED!"}
-  else {
+  else { 
+    return "I love you, too"
+  }
 }
 
 
